47c6d97c5f9efee31fdabad3bf9315768b93f9a2
Allan Dawid <allan.queiroz.dev@gmail.com>
2025-10-11 15:34:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { TabButton, TabContent, Tabs } from "./components/Tabs";
+import { Header } from "./components/Header";
+import { EmAsteriscos } from "./screens/EmAsteriscos";
+
+function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClickTab = (index: number) => setCurrentIndex(index);
+
+  return (
+    <div>
+      <Header />
+
+      <Tabs currentIndex={currentIndex} onClick={handleClickTab}>
+        <div>
+          <TabButton index={0}>Asterisco</TabButton>
+          <TabButton index={1}>Outra coisa</TabButton>
+        </div>
+
+        <TabContent index={0}>
+          <EmAsteriscos />
+        </TabContent>
+        <TabContent index={1}>Componente 2</TabContent>
+      </Tabs>
+    </div>
+  );
+}
+
+export default App;
