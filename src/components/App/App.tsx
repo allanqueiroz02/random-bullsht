87d6d4ebfafb75a98d6f0ad848b7a55faa50bd2d
Allan Dawid <allan.queiroz.dev@gmail.com>
2025-10-16 20:47:09 -0300

    refactor: ajusted main container in app and styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import { Header } from "../Header";
 import { TabButton, TabContent, Tabs } from "../Tabs";
 import { EmAsteriscos } from "../../pages/EmAsteriscos";
 
+import "./styles.css";
+
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -13,17 +15,19 @@ function App() {
     <div>
       <Header />
 
-      <Tabs currentIndex={currentIndex} onClick={handleClickTab}>
-        <div>
-          <TabButton index={0}>Asterisco (*)</TabButton>
-          <TabButton index={1}>Outra coisa</TabButton>
-        </div>
+      <main className="container-main">
+        <Tabs currentIndex={currentIndex} onClick={handleClickTab}>
+          <div>
+            <TabButton index={0}>Asterisco (*)</TabButton>
+            <TabButton index={1}>Outra coisa</TabButton>
+          </div>
 
-        <TabContent index={0}>
-          <EmAsteriscos />
-        </TabContent>
-        <TabContent index={1}>Componente 2</TabContent>
-      </Tabs>
+          <TabContent index={0}>
+            <EmAsteriscos />
+          </TabContent>
+          <TabContent index={1}>Componente 2</TabContent>
+        </Tabs>
+      </main>
     </div>
   );
 }
