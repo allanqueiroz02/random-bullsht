cae22736acab1e779888837304ec16a74da3c359
Allan Dawid <allan.queiroz.dev@gmail.com>
2025-11-03 22:55:46 -0300

    feat: add theme icon & initial style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,23 @@
-import { GiThrownDaggers  } from "react-icons/gi";
+import { GiThrownDaggers } from "react-icons/gi";
+import { VscColorMode } from "react-icons/vsc";
 
 import "./style.css";
 
 export const Header = () => {
+  function handleClickTheme() {
+    console.log("clicou no ícone");
+  }
+
   return (
     <div className="container-header">
       <h1 className="header">
-        Random Bullshit Go!!!! <GiThrownDaggers  />
+        Random Bullshit Go!!!! <GiThrownDaggers />
       </h1>
+      <VscColorMode
+        className="theme-icon"
+        title="Alterar tema"
+        onClick={handleClickTheme}
+      />
     </div>
   );
 };
