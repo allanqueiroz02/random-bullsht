88ff16c5c6e21b7c234da341cf87483f88b2ecd9
Allan Dawid <allan.queiroz.dev@gmail.com>
2025-11-19 21:16:24 -0300

    feat: new tab and typewriter effect

---
+++ src/pages/Random/Random.tsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+export const Random = () => {
+  return (
+    <div>
+      <h2>Typewriter Effect</h2>
+      <div className="typewriter-container">
+        <div>
+          <p className="typewriter-text">Texto de exemplo</p>
+        </div>
+      </div>
+    </div>
+  );
+};
