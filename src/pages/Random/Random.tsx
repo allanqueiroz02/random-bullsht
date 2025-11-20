3c646b7b02c04cb7483c57f6901f85a57e1da95e
Allan Dawid <allan.queiroz.dev@gmail.com>
2025-11-19 21:56:45 -0300

    feat: add pulse effect

--- src/pages/Random/Random.tsx
+++ src/pages/Random/Random.tsx
@@ -3,11 +3,17 @@ import "./style.css";
 export const Random = () => {
   return (
     <div>
-      <h2>Typewriter Effect</h2>
-      <div className="typewriter-container">
-        <div>
-          <p className="typewriter-text">Texto de exemplo</p>
+      <div>
+        <h2>Typewriter Effect</h2>
+        <div className="typewriter-container">
+          <div>
+            <p className="typewriter-text">Texto de exemplo</p>
+          </div>
         </div>
+      </div>
+      <div>
+        <h2>Pulse Effect</h2>
+        <div className="pulse"></div>
       </div>
     </div>
   );
